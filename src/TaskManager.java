dfc3fd4a008fa470ebd6fc8763775587049b49a2
Алексей Миронов <alexeymironov1991@gmail.com>
2023-01-28 12:49:36 +0300

    Проверка

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -3,45 +3,32 @@ import java.util.ArrayList;
 public interface TaskManager {
 
     void createTask(Task task);
-
     void createEpicTask(EpicTask epicTask);
-
     void createSubTask(SubTask subTask);
 
     // Получаем списки задач.
     ArrayList<Task> getTaskList();
-
     ArrayList<EpicTask> getEpicTaskList();
-
     ArrayList<SubTask> getSubTaskList();
-
     ArrayList<SubTask> getSubTaskListByEpic(EpicTask epicTask);
 
     //Удаляем задачи
     void clearTasks();
-
     void clearEpicTasks();
-
     void clearSubTasks();
 
     // Получаем по идентификатору
     Task getTaskById(int id);
-
     EpicTask getEpicTaskById(int id);
-
     SubTask getSubTaskById(int id);
 
     //Обновление задач
     void updateTask(Task task);
-
     void updateSubTask(SubTask subTask);
-
     void updateEpicTask(EpicTask epicTask);
 
     //Удаляем задачи
     void removeTask(int id);
-
     void removeEpicTask(int id);
-
     void removeSubTask(int id);
 }
